bac0a35e0742580739aec240033b5fd2a74ccd54
michel-heon <heon.michel@uqam.ca>
2022-03-11 05:26:04 -0500

    initial commit

---
+++ bundles/ca.uqam.dspace2vivo.model.openapi/00-env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+###################################################################
+# Script Name   :
+# Description   :
+# Args          : 
+# Author       	: Michel Héon PhD
+# Institution   : Université du Québec à Montréal
+# Copyright     : Université du Québec à Montréal (c) 2022
+# Email         : heon.michel@uqam.ca
+###################################################################
+export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
