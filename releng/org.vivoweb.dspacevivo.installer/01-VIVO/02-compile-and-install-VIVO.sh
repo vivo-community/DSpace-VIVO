53f98217ff7d7335269f7c30478d81acd79edc92
michel-heon <heon.michel@uqam.ca>
2022-04-13 17:45:43 -0400

    Prepare merge with dev-heon branch

--- releng/org.vivoweb.dspacevivo.installer/01-VIVO/02-compile-and-install-VIVO.sh
+++ releng/org.vivoweb.dspacevivo.installer/01-VIVO/02-compile-and-install-VIVO.sh
@@ -12,4 +12,5 @@
 export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd -P)"
 source $SCRIPT_DIR/../00-env.sh
 cd $DSPACEVIVO_HOME
+mkdir -p $VIVO_HOME
 mvn -T2C clean package install -DskipTests -Pdefaults -s installer/settings.xml
