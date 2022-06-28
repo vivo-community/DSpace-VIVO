9827fdbeb9c832d7faa11d4d190045c67fc0f570
michel-heon <heon.michel@uqam.ca>
2022-06-28 10:49:02 -0400

    Bug corection

--- releng/org.vivoweb.dspacevivo.aws.beanstalk/aws-platform/.platform/hooks/prebuild/05-run-etl.sh
+++ releng/org.vivoweb.dspacevivo.aws.beanstalk/aws-platform/.platform/hooks/prebuild/05-run-etl.sh
@@ -9,5 +9,5 @@
 # Copyright     : Université du Québec à Montréal (c) 2022
 # Email         : heon.michel@uqam.ca
 ###################################################################
-sleep 15m ; at now -f /usr/local/bin/etl_script.sh &
+at now + 15 minute -f /usr/local/bin/etl_script.sh
 
