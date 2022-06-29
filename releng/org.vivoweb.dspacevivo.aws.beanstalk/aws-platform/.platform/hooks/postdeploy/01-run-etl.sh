c0430ec36c2d78e4a5198645b3af046b6dac90cc
michel-heon <heon.michel@uqam.ca>
2022-06-29 06:36:52 -0400

    Fix config

--- releng/org.vivoweb.dspacevivo.aws.beanstalk/aws-platform/.platform/hooks/postdeploy/01-run-etl.sh
+++ releng/org.vivoweb.dspacevivo.aws.beanstalk/aws-platform/.platform/hooks/postdeploy/01-run-etl.sh
@@ -9,5 +9,5 @@
 # Copyright     : Université du Québec à Montréal (c) 2022
 # Email         : heon.michel@uqam.ca
 ###################################################################
-(sleep 3m && /usr/local/bin/etl_script.sh) &
-
+/usr/local/bin/etl_script.sh &
+echo "/usr/local/bin/etl_script.sh is send " > /tmp/run-etl.log 
